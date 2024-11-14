efb71be46c68376819fb3c5b8116148a64727dc0
IzotovAA <izotovandreiy@gmail.com>
2024-11-14 14:19:11 +0300

    init commit

---
+++ MinStack.php
@@ -0,0 +1,39 @@
+<?php
+
+class MinStack
+{
+    /**
+     */
+    function __construct() {
+
+    }
+
+    /**
+     * @param Integer $val
+     * @return NULL
+     */
+    function push($val) {
+
+    }
+
+    /**
+     * @return NULL
+     */
+    function pop() {
+
+    }
+
+    /**
+     * @return Integer
+     */
+    function top() {
+
+    }
+
+    /**
+     * @return Integer
+     */
+    function getMin() {
+
+    }
+}
